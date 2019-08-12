1bd8bb6de9590edbd868de88f18b81449c587b05
Caleb Spain <crspain@uh.edu>
2019-08-11 19:37:36 -0500

    initial commit

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.sass']
+})
+export class NavComponent implements OnInit {
+
+  mobile: boolean = false;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  toggleMobile() {
+    this.mobile = !this.mobile;
+  }
+
+}
